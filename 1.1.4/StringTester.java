88b11c34adaf0a3b9040ceeb6911e42436bd6c77
Burton <KB145350@davenportschools.org>
2019-02-21 10:57:40 -0600

    update

---
+++ 1.1.4/StringTester.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class StringTester here.
+ *
+ * @author (Kyle Burton)
+ * @version (2/21/19)
+ */
+public class StringTester
+{
+    public static void main(){
+        System.out.println(WeatherConditionalas.getWeatherAdvice(32, "heavy snow"));
+    }
+}
